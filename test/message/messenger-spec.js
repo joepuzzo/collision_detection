56b3b75f231ed489347e6af95365620ff7a80305
joepuzzo <jgpuzzo@metrocast.net>
2016-02-10 17:03:31 -0500

    Fixed messenger unit test

--- test/message/messenger-spec.js
+++ test/message/messenger-spec.js
@@ -7,45 +7,69 @@ describe("Messenger", function() {
       
   describe("mail()", function() { 
 
+        // Variables for testing
+        var sender, reciever, notreciever; 
+
         // Create sender and reciver
-        var sender = new Messenger( "A" );
-        var reciever = new Messenger( "B" );
-        var notreciever = new Messenger( "C" );
+        beforeEach( function() { 
+            sender = new Messenger( "A" );
+            reciever = new Messenger( "B" );
+            notreciever = new Messenger( "C" );
+        });
+
+        afterEach( function(){
+            sender.close();
+            reciever.close(); 
+            notreciever.close();
+        });
 
         // Create a hia messag 
         var hia = new Messages.HIA(2,3,4);
+
+        this.timeout(5000); 
             
         it("Sender should send evelope and reciever should recieve it", function(done) {
             
             reciever.once( "hia", function( msg, from ) { 
-                //console.log( JSON.stringify( msg ) );
-                expect( JSON.stringify( hia ) ).to.equal( '{"type":"hia","lat":2,"lon":3,"alt":4}' ); 
-                done(); 
+                expect( JSON.stringify( msg ) ).to.equal( '{"type":"hia","lat":2,"lon":3,"alt":4}' ); 
+                done();
             });
 
             sender.sendLocal( [hia] );
         });
 
-
         it("A should send evelope and B should revieve it", function(done) {
 
             reciever.once( "hia", function( msg, from ) {
-                expect( JSON.stringify( hia ) ).to.equal( '{"type":"hia","lat":2,"lon":3,"alt":4}' );
-		        done();
+                expect( JSON.stringify( msg ) ).to.equal( '{"type":"hia","lat":2,"lon":3,"alt":4}' );
             });
 		
 	        notreciever.once( "hia", function( msg, from ) {
-		        console.log("THIS SHOULD NOT HAPPEN!!!");
-                expect( JSON.stringify( hia ) ).to.not.equal( '{"type":"hia","lat":2,"lon":3,"alt":4}' );
+                expect( JSON.stringify( msg ) ).to.not.equal( '{"type":"hia","lat":2,"lon":3,"alt":4}' );
             });
 		
             sender.sendLocal( [hia], {id: "B"} );
+
+            setTimeout(function(){ done(); }, 2000);
         });
 
+        it("A should send evelope and both B and C should revieve it", function(done) {
 
-        after( function() { 
-            sender.close();
-            reciever.close(); 
+            reciever.once( "hia", function( msg, from ) {
+                expect( JSON.stringify( msg ) ).to.equal( '{"type":"hia","lat":2,"lon":3,"alt":4}' );
+            });
+		
+	        notreciever.once( "hia", function( msg, from ) {
+                expect( JSON.stringify( msg ) ).to.equal( '{"type":"hia","lat":2,"lon":3,"alt":4}' );
+            });
+
+            sender.once( "hia", function( msg, from ) {
+                expect( JSON.stringify( msg ) ).to.not.equal( '{"type":"hia","lat":2,"lon":3,"alt":4}' );
+            });
+		
+            sender.sendLocal( [hia] );
+ 
+            setTimeout(function(){ done(); }, 2000);
         });
 
     });
