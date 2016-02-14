ae6aa78210b98cbcd7c10fc06f8782795e0290e5
cpepin <pepin.cameron@gmail.com>
2016-02-14 15:14:50 -0500

    Completed ack functionality, tested (Test file not working with all tests)

--- test/message/messages-spec.js
+++ test/message/messages-spec.js
@@ -3,41 +3,43 @@ var expect   = require('chai').expect,
     Messages = require('../../lib/message/messages.js');
 
 describe("Message", function() {
-      
-  describe("HIA", function() { 
+
+  describe("HIA", function() {
 
         var hia;
-            
+
         beforeEach( function() {
             hia = new Messages.HIA(2,3,4);
         });
 
         it("should eqal the test message", function(done) {
-            expect( JSON.stringify( hia) ).to.equal( '{"type":"hia","ack":false,"lat":2,"lon":3,"alt":4}' ); 
-            done(); 
+            expect( JSON.stringify( hia) ).to.equal( '{"type":"hia","ack":false,"lat":2,"lon":3,"alt":4}' );
+            done();
         });
 
         it("should throw an error due to incorrect type", function(done) {
 
-            var f = function() { 
-                Messages.HIA.validate( JSON.parse('{ "type": "hia", "ack": false, "lat": 7, "lon": 6, "alt": "hi" }') ); 
+            var f = function() {
+                Messages.HIA.validate( JSON.parse('{ "type": "hia", "ack": false, "lat": 7, "lon": 6, "alt": "hi" }') );
             }
             expect( f ).to.throw(TypeError);
-            done(); 
+            done();
 
         });
 
         it("should throw an error due to missing data", function(done) {
 
-            var f = function() { 
+            var f = function() {
                 Messages.HIA.validate( JSON.parse('{ "type": "hia", "lat": 7, "lon": 6 }') );
             }
             expect( f ).to.throw(Error);
-            done(); 
+            done();
 
         });
 
     });
+
+
 });
 
 
@@ -51,15 +53,15 @@ describe("Message", function() {
 
 
 
-/*try { 
+/*try {
     hia.validate( JSON.parse('{ "type": "hia", "lat": 7, "lon": 6, "alt": 1 }') );
     hia.validate( JSON.parse('{ "type": "hia", "lat": 7, "lon": 6, "alt": "hi" }') );
-} catch( e ) { 
+} catch( e ) {
     console.log( e.message );
 }
 
-try { 
-} catch( e ) { 
+try {
+} catch( e ) {
     console.log( e.message );
 }
 
--- test/message/messenger-spec.js
+++ test/message/messenger-spec.js
@@ -1,37 +1,37 @@
 // Require an assersion library
 var expect    = require('chai').expect,
-    Messages = require('../../lib/message/messages.js'), 
+    Messages = require('../../lib/message/messages.js'),
     Messenger = require('../../lib/message/messenger.js');
 
 describe("Messenger", function() {
-      
-  describe("mail()", function() { 
+
+  describe("mail()", function() {
 
         // Variables for testing
-        var sender, reciever, notreciever; 
+        var sender, reciever, notreciever, hia;
 
         // Create sender and reciver
-        beforeEach( function() { 
+        beforeEach( function() {
             sender = new Messenger( "A" );
             reciever = new Messenger( "B" );
             notreciever = new Messenger( "C" );
+
+            // Create a hia messag
+            hia = new Messages.HIA(2,3,4);
         });
 
         afterEach( function(){
             sender.close();
-            reciever.close(); 
+            reciever.close();
             notreciever.close();
         });
 
-        // Create a hia messag 
-        var hia = new Messages.HIA(2,3,4);
+        this.timeout(5000);
 
-        this.timeout(5000); 
-            
         it("Sender should send evelope and reciever should recieve it", function(done) {
-            
-            reciever.once( "hia", function( msg, from ) { 
-                expect( JSON.stringify( msg ) ).to.equal( '{"type":"hia","ack":false,"lat":2,"lon":3,"alt":4}' ); 
+
+            reciever.once( "hia", function( msg, from ) {
+                expect( JSON.stringify( msg ) ).to.equal( '{"type":"hia","ack":false,"lat":2,"lon":3,"alt":4}' );
                 done();
             });
 
@@ -43,11 +43,11 @@ describe("Messenger", function() {
             reciever.once( "hia", function( msg, from ) {
                 expect( JSON.stringify( msg ) ).to.equal( '{"type":"hia","ack":false,"lat":2,"lon":3,"alt":4}' );
             });
-		
+
 	        notreciever.once( "hia", function( msg, from ) {
                 expect( JSON.stringify( msg ) ).to.not.equal( '{"type":"hia","ack":false,lat":2,"lon":3,"alt":4}' );
             });
-		
+
             sender.sendLocal( [hia], {id: "B"} );
 
             setTimeout(function(){ done(); }, 2000);
@@ -58,7 +58,7 @@ describe("Messenger", function() {
             reciever.once( "hia", function( msg, from ) {
                 expect( JSON.stringify( msg ) ).to.equal( '{"type":"hia","ack":false,"lat":2,"lon":3,"alt":4}' );
             });
-		
+
 	        notreciever.once( "hia", function( msg, from ) {
                 expect( JSON.stringify( msg ) ).to.equal( '{"type":"hia","ack":false,"lat":2,"lon":3,"alt":4}' );
             });
@@ -66,13 +66,74 @@ describe("Messenger", function() {
             sender.once( "hia", function( msg, from ) {
                 expect( JSON.stringify( msg ) ).to.not.equal( '{"type":"hia","ack":false,"lat":2,"lon":3,"alt":4}' );
             });
-		
+
             sender.sendLocal( [hia] );
- 
+
             setTimeout(function(){ done(); }, 2000);
         });
 
+        describe('--ack--', function() {
+
+          var marco1, marco2;
+
+          beforeEach( function() {
+            marco1 = new Messages.MARCO();
+            marco2 = new Messages.MARCO();
+          });
+
+          it ('should recieve an ack packet in response to marco', function(done) {
+            sender.once( 'ack' + marco1.ack, function( msg, from ) {
+                expect(from.id).to.equal('B');
+                done();
+            });
+
+
+            sender.sendLocal([marco1], {id: 'B'});
+           });
+
+          it('should discern multiple ack packets', function(done) {
+            var i = 0;
+            sender.once('ack' + marco1.ack, function( msg, from ) {
+              expect(from.id).to.equal('B');
+              i++;
+              if ( i === 2 ) done();
+            });
+            sender.once('ack' + marco2.ack, function( msg, from ) {
+              expect(from.id).to.equal('C');
+              i++;
+              if ( i === 2 ) done();
+            });
+
+
+            sender.sendLocal([marco1], {id: 'B'});
+            sender.sendLocal([marco2], {id: 'C'});
+          });
+
+          it('should be able to recieve multiple acks from same reciever', function(done) {
+            var i = 0;
+            sender.once('ack' + marco1.ack, function( msg, from ) {
+              expect(from.id).to.equal('B');
+              i++;
+              if ( i === 2 ) done();
+            });
+            sender.once('ack' + marco2.ack, function( msg, from ) {
+              expect(from.id).to.equal('B');
+              i++;
+              if ( i === 2 ) done();
+            });
+
+
+            sender.sendLocal([marco1], {id: 'B'});
+            sender.sendLocal([marco2], {id: 'B'});
+          });
+
+
+
+
+        });
+
     });
+
 });
 
 
@@ -86,15 +147,15 @@ describe("Messenger", function() {
 
 
 
-/*try { 
+/*try {
     hia.validate( JSON.parse('{ "type": "hia", "lat": 7, "lon": 6, "alt": 1 }') );
     hia.validate( JSON.parse('{ "type": "hia", "lat": 7, "lon": 6, "alt": "hi" }') );
-} catch( e ) { 
+} catch( e ) {
     console.log( e.message );
 }
 
-try { 
-} catch( e ) { 
+try {
+} catch( e ) {
     console.log( e.message );
 }
 
